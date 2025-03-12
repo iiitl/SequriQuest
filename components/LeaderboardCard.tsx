b01efb757d1e629136c1535936449d6b9ae2c238
Om Swami <reach.oms2004@gmail.com>
2025-03-12 17:00:34 +0530

    Fix Build Errors

--- components/LeaderboardCard.tsx
+++ components/LeaderboardCard.tsx
@@ -1,6 +1,13 @@
 "use client";
 
-export default function LeaderboardCard({ rank, username, solved, time }) {
+interface LeaderboardCardProps {
+  rank: number;
+  username: string;
+  solved: number;
+  time: string;
+}
+
+export default function LeaderboardCard({ rank, username, solved, time }: LeaderboardCardProps) {
   return (
     <div className="bg-black border border-green-600 shadow-md p-6 rounded-lg text-green-400 font-mono text-lg flex justify-between">
       <span className="text-green-300">#{rank}</span>
