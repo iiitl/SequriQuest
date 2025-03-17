9529ebab9b64a827760e492f0caa11b094a8a01f
Om Swami <reach.oms2004@gmail.com>
2025-03-18 00:18:56 +0530

    disable challenges for all

--- app/(root)/challenges/page.tsx
+++ app/(root)/challenges/page.tsx
@@ -3,8 +3,8 @@
 import QuestCard from "@/components/QuestCard";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
-// import { useEffect, useState } from "react";
-// import { useRouter } from "next/navigation";
+ import { useEffect, useState } from "react";
+ import { useRouter } from "next/navigation";
 const quests = [
   { title: "Challenge 1", points: 50, description: "Pointy pointers.", hint: "Is the source code in C?", id: "c1" },
   { title: "Challenge 2", points: 75, description: "Disk Diving.", hint: "Dive deep to find the best gems.", id: "c2" },
@@ -14,9 +14,9 @@ const quests = [
 ];
 
 export default function Challenges() {
-//  const [isLoading, setIsLoading] = useState(true);
- // const router = useRouter();
-/**
+  const [isLoading, setIsLoading] = useState(true);
+   const router = useRouter();
+
   useEffect(() => {
     const checkAuth = async () => {
       try {
@@ -48,7 +48,7 @@ export default function Challenges() {
       </>
     );
   }
-  **/
+
   
 
   return (
